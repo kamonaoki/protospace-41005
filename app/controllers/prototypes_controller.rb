2665771da87bc211d1139718d76a21d9447e26ef
kamonaoki <naokun616@gmail.com>
2024-06-14 17:58:58 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,60 @@
+class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, except: [:index, :show]
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def create
+    @prototype =Prototype.create(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @prototype.destroy
+    redirect_to root_path
+  end
+
+end
+
+private
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :image, :catch_copy, :concept).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
